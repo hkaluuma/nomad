2e4f1be2ae160229d5b03f83ad497041aaa78d51
hkaluuma <hkaluuma@idi.co.ug>
2023-05-19 19:10:24 +0300

    try login

---
+++ app/src/main/java/com/example/nomad/Config.java
@@ -0,0 +1,6 @@
+package com.example.nomad;
+
+public class Config {
+
+    public String server_ip = "10.0.2.2:80";
+}
--- app/src/main/java/com/example/nomad/LoginActivity.java
+++ app/src/main/java/com/example/nomad/LoginActivity.java
@@ -1,51 +1,38 @@
 package com.example.nomad;
-
-        import android.app.Activity;
-        import android.content.SharedPreferences;
-        import android.os.Bundle;
-        import com.google.android.material.floatingactionbutton.FloatingActionButton;
-        import com.google.android.material.snackbar.Snackbar;
-        import com.muddzdev.styleabletoast.StyleableToast;
-        import androidx.appcompat.app.AppCompatActivity;
-        import androidx.appcompat.widget.Toolbar;
-        import androidx.appcompat.app.AppCompatActivity;
-        import android.app.ProgressDialog;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.net.ConnectivityManager;
-        import android.net.NetworkInfo;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
-
-        import org.apache.http.HttpResponse;
-        import org.apache.http.NameValuePair;
-        import org.apache.http.client.entity.UrlEncodedFormEntity;
-        import org.apache.http.client.methods.HttpPost;
-        import org.apache.http.impl.client.DefaultHttpClient;
-        import org.apache.http.message.BasicNameValuePair;
-
-        import java.io.BufferedReader;
-        import java.io.InputStream;
-        import java.io.InputStreamReader;
-        import java.util.ArrayList;
-
-        import android.view.View;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpResponse;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.NameValuePair;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.entity.UrlEncodedFormEntity;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.methods.HttpPost;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.impl.client.DefaultHttpClient;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.message.BasicNameValuePair;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
-
     String username, pass;
-
-    String login_url= "https://maendeleotech.com/hbc/authentic.php";
-
+    //instantiate config class
+    Config config = new Config();
+    String login_url= "https://"+config.server_ip+"/nomad/login.php";
     //shared preferences variables
     SharedPreferences sharedpreferences;
-    SharedPreferences mySharedPreferences;
     public static final String MYPREFERENCES_LOGIN = "MyPreferences_002";
     SharedPreferences.Editor editor;
 
@@ -53,12 +40,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        //new codes
         final EditText editTextusername = findViewById(R.id.ed_username);
         final EditText editTextpassword = findViewById(R.id.ed_pwd);
         final Button btn1 = findViewById(R.id.login_btn);
         final Button btn2 = findViewById(R.id.forgot_password);
-
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,42 +53,31 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-        btn1 .setOnClickListener(new View.OnClickListener() {
-
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 pass = editTextpassword.getText().toString();
                 username = editTextusername.getText().toString();
-
                 if (username.isEmpty()) {
                     editTextusername.setError("User Name is Required");
-                }
-
-                if (pass.isEmpty()) {
+                }if (pass.isEmpty()) {
                     editTextpassword.setError("Password is required");
-
                 } else {
-
                     if (haveNetworkConnection()) {
                         // connected
-                        new LoginActivity.CreateLogin().execute();
+                        CreateLogin createLogin = new CreateLogin();
+                        createLogin.execute();
                     } else {
                         // not connected
                         Toast.makeText(LoginActivity.this, "No internet Connection", Toast.LENGTH_SHORT).show();
-
                     }
-
                 }
             }
         });
-
     }
 
-
     //method to check internet availability(WiFi and MobileData)
+    @SuppressWarnings("deprecation")
     private boolean haveNetworkConnection() {
         boolean haveConnectedWifi = false;
         boolean haveConnectedMobile = false;
@@ -119,56 +94,36 @@ public class LoginActivity extends AppCompatActivity {
         }
         return haveConnectedWifi || haveConnectedMobile;
     }
-
-
     //async task class to login
     class CreateLogin extends AsyncTask<String, String, String> {
-
         String responcefromphp;
         //create progress dialog class
         ProgressDialog pdialog;
 
-
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-
             //show dialog while registering Business
             pdialog = new ProgressDialog(LoginActivity.this);
-
-
             pdialog.setMessage("Please wait...");
             pdialog.setIndeterminate(false);//holds till process is done
             pdialog.setCancelable(false);// set screen in freez
             pdialog.show();
-
         }
 
+        @SuppressWarnings("deprecation")
         @Override
         protected String doInBackground(String... strings) {
-
             //upload data to the database
-
             try {
-
-                /* seting up the connection and send data with url */
-                // create a http default client - initialize the HTTp client
-
                 DefaultHttpClient httpclient = new DefaultHttpClient();
                 HttpPost httppost = new HttpPost(login_url);
-                //HttpPost httppost = new HttpPost("http://192.168.43.20:8081/hbc/login.php");
-
                 ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
-
                 nameValuePairs.add(new BasicNameValuePair("biz_username", username));
                 nameValuePairs.add(new BasicNameValuePair("biz_pass", pass));
-
-
                 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
                 HttpResponse response = httpclient.execute(httppost);
-
                 InputStream inputStream = response.getEntity().getContent();
-
                 BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream), 4096);
                 String line;
                 StringBuilder sb = new StringBuilder();
@@ -178,41 +133,22 @@ public class LoginActivity extends AppCompatActivity {
                 rd.close();
                 responcefromphp = sb.toString();
                 inputStream.close();
-
             } catch (Exception e) {
                 Toast.makeText(getApplicationContext(), "Try Again", Toast.LENGTH_LONG).show();
-
             }
-
             return responcefromphp;
-
-
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-
             // dismiss dialog and perform other tasks
             pdialog.dismiss();
-
-            /*if (responcefromphp.equals("1")) {
-
-                //.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                StyleableToast.makeText(LoginActivity.this, "Login Successful", R.style.exampleToast).show();
-                Intent loginintent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(loginintent);
-
-            }  */
             if(responcefromphp.equals("0")){
-
-                //Toast.makeText(LoginActivity.this, "Login Failed customer not Registered.", Toast.LENGTH_SHORT).show();
                 Toast.makeText(LoginActivity.this, "Login Failed customer not Registered.", Toast.LENGTH_SHORT).show();
-
             } else {
                 String[] usercredentials = responcefromphp.split("#");
                 Log.e("responcefromphp", responcefromphp);
-
                 SharedPreferences mySharedPreferences = getSharedPreferences(MYPREFERENCES_LOGIN, Activity.MODE_PRIVATE);
                 editor = mySharedPreferences.edit();
                 editor.putString("username", usercredentials[0]);
@@ -222,19 +158,10 @@ public class LoginActivity extends AppCompatActivity {
                 editor.putString("email", usercredentials[4]);
                 editor.putString("id", usercredentials[5]);
                 editor.commit();
-
-                //Toast.makeText(LoginActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
                 Toast.makeText(LoginActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
-
             }
-
         }
-
     }//end of asnck task
-
-
-
-
 }
